ad36a1bcb45f1f76c6dc5d7608b81484944a5a77
Habib <habib.alhabib@esprit.tn>
2022-10-02 23:47:57 +0100

    Add bootstrap and font-awesome libraries as well as adding countries component

--- hr-management/src/app/app.module.ts
+++ hr-management/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountriesComponent } from './countries/countries.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountriesComponent
   ],
   imports: [
     BrowserModule
