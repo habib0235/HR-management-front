145ebbe78601ad2b0191676e353ab2b49eca4c0c
Habib <habib.alhabib@esprit.tn>
2022-10-08 23:52:21 +0100

    Changed countries list component as to display countries in a table as well as adding filtering feature to it

--- hr-management/src/app/countries/countries.component.ts
+++ hr-management/src/app/countries/countries.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { COUNTRIES } from '../mock-countries';
+import { Country } from '../model/country';
 
 
 @Component({
@@ -9,13 +10,30 @@ import { COUNTRIES } from '../mock-countries';
 })
 export class CountriesComponent implements OnInit {
 
-
-
   countries = COUNTRIES;
+  filteredCountries = this.countries;
+
+  private _filter = "";
+
+  get filter(): string {
+    return this._filter;
+  }
+
+  set filter(value: string) {
+    console.log("In setter : " + this.filter);
+    this._filter = value;
+    this.filteredCountries = this.filterCountries();
+  }
+
+
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  filterCountries() {
+    return this.countries.filter((country: Country) => country.countryName.toLocaleLowerCase().includes(this.filter.toLocaleLowerCase()));
   }
 
 }
